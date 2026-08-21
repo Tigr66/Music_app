a2e466a3d374365c428b272e3e91db0293c544ac
Tigran Gareev <gareevtigran@gmail.com>
2026-08-21 20:57:13 +0500

    #23 Добавил notifyService

---
+++ client/src/services/notify.service.ts
@@ -0,0 +1,17 @@
+import { toast } from "react-toastify";
+
+export const notifySuccess = (message: string) => {
+    toast.success(message);
+};
+
+export const notifyError = (message: string) => {
+    toast.error(message);
+};
+
+export const notifyInfo = (message: string) => {
+    toast.info(message);
+};
+
+export const notifyWarning = (message: string) => {
+    toast.warning(message);
+};
