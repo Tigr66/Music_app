e6ac2b743c728c5bd780da557dc8d485a3f62b28
Tigran Gareev <gareevtigran@gmail.com>
2026-09-12 16:54:34 +0500

    #25 Реализовал метод Delete в StorageService

--- server/src/services/storage.service.ts
+++ server/src/services/storage.service.ts
@@ -1,17 +1,18 @@
 import * as Minio from "minio";
 import { minioClient } from "../lib/minio";
-import { UploadFileData } from "../types/storage.types";
+import { DeleteFileData, UploadFileData } from "../types/storage.types";
 import { minioConfig } from "../config/minio.config";
 import { InternalServerError } from "../errors/internal-server-error";
 import { randomUUID } from "crypto";
-import { BadRequestError } from "../errors/bad-request-error";
 import { fileExtensions } from "../config/file-extensions.config";
 
 export class StorageService {
     private minioClient: Minio.Client;
+    private bucketName: string;
 
     constructor() {
         this.minioClient = minioClient;
+        this.bucketName = minioConfig.bucket;
     }
 
     async upload(file: UploadFileData): Promise<string> {
@@ -22,7 +23,7 @@ export class StorageService {
         }
 
         try {
-            const bucketName = file.bucketName ?? minioConfig.bucket;
+            const bucketName = file.bucketName ?? this.bucketName;
 
             const objectName = `${file.folder ? `${file.folder}/` : ""}${randomUUID()}${extension}`;
 
@@ -39,6 +40,50 @@ export class StorageService {
             return objectName;
         } catch (error) {
             throw new InternalServerError("Failed to upload file to storage");
+        }
+    }
+
+    async delete(fileData: DeleteFileData): Promise<void> {
+        try {
+            const bucketName = fileData.bucketName ?? this.bucketName;
+
+            const exists = await this.exists({
+                bucketName,
+                objectName: fileData.objectName,
+            });
+
+            if (!exists) {
+                return;
+            }
+
+            await this.minioClient.removeObject(
+                bucketName,
+                fileData.objectName,
+            );
+        } catch (error) {
+            throw new InternalServerError("Failed to delete file from storage");
+        }
+    }
+
+    async exists(fileData: DeleteFileData): Promise<boolean> {
+        try {
+            await this.minioClient.statObject(
+                fileData.bucketName ?? this.bucketName,
+                fileData.objectName,
+            );
+
+            return true;
+        } catch (error) {
+            if (
+                typeof error === "object" &&
+                error !== null &&
+                "code" in error &&
+                error.code === "NotFound"
+            ) {
+                return false;
+            }
+
+            throw new InternalServerError("Failed to check file in storage");
         }
     }
 }
--- server/src/types/storage.types.ts
+++ server/src/types/storage.types.ts
@@ -4,3 +4,8 @@ export type UploadFileData = {
     buffer: Buffer;
     contentType: string;
 };
+
+export type DeleteFileData = {
+    bucketName?: string;
+    objectName: string;
+};
