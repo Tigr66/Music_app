c3bbef211c70c50ce75f1f03d3efdb81f29f19ab
Tigran Gareev <gareevtigran@gmail.com>
2026-09-12 16:35:36 +0500

    #25 Реализовал config для расширений файлов

---
+++ server/src/config/file-extensions.config.ts
@@ -0,0 +1,13 @@
+export const fileExtensions: Record<string, string> = {
+    // Images
+    "image/jpeg": ".jpg",
+    "image/png": ".png",
+    "image/webp": ".webp",
+    "image/svg+xml": ".svg",
+
+    // Audio
+    "audio/mpeg": ".mp3",
+    "audio/wav": ".wav",
+    "audio/ogg": ".ogg",
+    "audio/flac": ".flac",
+};
